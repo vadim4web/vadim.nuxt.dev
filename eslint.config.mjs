1cfecb668ccdaae1b4c998ef48b8f307771d7f35
vadim4web <vadim4web@gmail.com>
2025-03-05 17:47:34 +0200

    add linter rules, save before lint all

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,31 @@ export default withNuxt(
   // Your custom configs here
 ).overrideRules({
   'vue/max-attributes-per-line': ['warn', { singleline: 3 }],
+  'vue/singleline-html-element-content-newline': 'off',
+  'vue/multiline-html-element-content-newline': 'off',
+  'vue/html-indent': 'off',
+  'vue/html-self-closing': 'off',
+  'vue/require-default-prop': 'off',
+  'vue/no-unused-vars': ['error', {
+    ignorePattern: '^_',
+  }],
+  'vue/attributes-order': [
+    'error',
+    {
+      order: [
+        'DEFINITION',
+        'LIST_RENDERING',
+        'CONDITIONALS',
+        'RENDER_MODIFIERS',
+        'GLOBAL',
+        ['UNIQUE', 'SLOT'],
+        'TWO_WAY_BINDING',
+        'OTHER_DIRECTIVES',
+        'OTHER_ATTR',
+        'EVENTS',
+        'CONTENT',
+      ],
+      alphabetical: false,
+    },
+  ],
 })
